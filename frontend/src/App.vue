e21ff357af5105ff52c6b5350cd809eb232eb89a
sly20020806w <691262631@qq.com>
2026-09-14 09:06:46 +0800

    fix: 恢复登录外导航栏布局

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,15 @@
 <template>
-  <router-view />
+  <div v-if="route.path === '/login'">
+    <router-view />
+  </div>
+  <div v-else>
+    <NavBar />
+    <router-view />
+  </div>
 </template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+import NavBar from './components/NavBar.vue'
+const route = useRoute()
+</script>
